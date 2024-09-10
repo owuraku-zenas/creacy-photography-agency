5993b91da9f407ca506783df569d27c90cf0e38e
Owuraku Zenas <awukuzenas@gmail.com>
2024-09-10 20:56:08 +0000

    responsive expertise

--- src/sections/Expertise.tsx
+++ src/sections/Expertise.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 const Expertise = (props: Props) => {
     return (
-        <div className='flex h-full min-h-screen w-full max-w-7xl flex-col bg-[#363733] p-8 md:rounded-2xl'>
-            <div className="flex justify-between">
-                <div className="flex w-full max-w-xl flex-col gap-8">
+        <div className='flex h-full min-h-screen w-full max-w-7xl flex-col gap-6 bg-[#363733] md:rounded-2xl md:p-8'>
+            <div className="flex flex-col justify-between md:flex-row">
+                <div className="flex w-full max-w-xl flex-col gap-8 p-8">
                     <div className="flex flex-col">
                         <p className='text-[#90908F]'>{"(02)"}</p>
                         <p className='text-white'>Our Expertise</p>
@@ -15,8 +15,8 @@ const Expertise = (props: Props) => {
                         When moments capture every dream crafted into beautiful reality
                     </p>
                 </div>
-                <div className="right flex w-full flex-col items-center p-6 text-2xl md:max-w-xs">
-                    <ul className='flex flex-col gap-7 text-left text-[#828380]'>
+                <div className="right flex w-full flex-col items-center overflow-x-scroll px-2 text-2xl md:max-w-xs md:p-6">
+                    <ul className='flex gap-7 py-3 text-left text-[#828380] md:flex-col'>
                         <li className='cursor-pointer text-[#FCFCFC] transition-colors duration-300 ease-in-out'>Landscape</li>
                         <li className='cursor-pointer transition-colors duration-300 ease-in-out hover:text-[#FCFCFC]'>Wildlife</li>
                         <li className='cursor-pointer transition-colors duration-300 ease-in-out hover:text-[#FCFCFC]'>Architectural</li>
