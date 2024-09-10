3c7142750db728fafa48674b30dc81c06e36795f
Owuraku Zenas <awukuzenas@gmail.com>
2024-09-10 18:11:19 +0000

    Hero Structure

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+  return (
+    <div className='relative z-0 flex h-[calc(100vh-4rem)] w-full flex-col overflow-hidden md:rounded-2xl'>
+        <div  className='absolute bottom-0 left-0 right-0 top-0 -z-20 bg-transparent/50 bg-[url("/images/hero2.jpg")] bg-cover' />
+        <div  className='absolute bottom-0 left-0 right-0 top-0 bg-transparent/50' />
+        <div className="flex-[1]">Nav</div>
+        <div className="flex-[5]">Main Area</div>
+        <div className="flex-[3]">Bottom</div>
+    </div>
+  )
+}
+
+export default Hero
